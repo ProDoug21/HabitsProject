d0616e545a0e31a59a36b0db8d2f5be450fb8b6d
Douglas <jose4030@outlook.com.br>
2023-01-25 00:31:06 -0300

    Adicionando e Salvando as Tarefas

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: ["01-01", "01-02", "01-06","01-07","01-08"],
-  takePills: ["01-06"],
-  Journay: ["01-02"],
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso🧨")
+    return
+  }
+
+  alert("Adicionado com Sucesso 💚")
+  nlwSetup.addDay(today)
 }
 
+function save() {
+  localStorage.setItem("NLWSETUP@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSETUP@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
